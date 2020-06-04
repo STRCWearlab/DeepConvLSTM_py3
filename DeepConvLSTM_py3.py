b93392403cf3475e829a24c91fbce52247a978e3
Lloyd Pellatt <PellattLloyd@gmail.com>
2020-06-04 20:01:55 +0100

    Updated to work with new iterate_minibatches_2D, and changed optimiser to Adam

--- DeepConvLSTM_py3.py
+++ DeepConvLSTM_py3.py
@@ -1,49 +1,39 @@
-
 import torch
-import warnings
-warnings.filterwarnings('ignore')
-import numpy as np 
-
+import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd 
 import sklearn.metrics as metrics
 from sklearn.metrics import classification_report,confusion_matrix
 from datetime import datetime
-import time
-import os
-import math 
 from torch import nn
-import torch.nn.functional as F 
 from utils import *
 import csv
 from pytorchtools import EarlyStopping
-
+import seaborn as sn
 
 # Define constants
-n_channels = 67 # number of sensor channels
-len_seq = 32 # Sliding window length MUST BE A MULTIPLE OF 4 +1
+n_channels = 113 # number of sensor channels
+len_seq = 32 # Sliding window length
 stride = 1 # Sliding window step
 num_epochs = 300 # Max no. of epochs to train for
-num_batches= -1 # No. of batches per epoc. -1 means all windows will be presented at least once, up to batchlen times per epoch (unless undersampled)
-batch_size = 1000 # Batch size / width - this many sequential batches will be processed at once
-patience= 50 # Patience of early stopping routine. If criteria does not decrease in this many epochs, training is stopped.
-batchlen = 10 # No. of consecutive
+num_batches= 20 # No. of training batches per epoch. -1 means all windows will be presented at least once, up to batchlen times per epoch (unless undersampled)
+batch_size = 1000 # Batch size / width - this many windows of data will be processed at once
+patience= 75 # Patience of early stopping routine. If criteria does not decrease in this many epochs, training is stopped.
+batchlen = 200 # No. of consecutive windows in a batch. If false, the largest number of windows possible is used.
 val_batch_size = 10000 # Batch size for validation/testing. Useful to make this as large as possible given GPU memory, to speed up validation and testing.
 test_batch_size = 10000
+lr = 0.0001 # Initial (max) learning rate
+num_batches_val = 2 # How many batches should we validate on each epoch
 
 
-
-
+# Class names for opportunity dataset
 opp_class_names = ['Null','Open Door 1','Open Door 2','Close Door 1','Close Door 2','Open Fridge',
 'Close Fridge','Open Dishwasher','Close Dishwasher','Open Drawer 1','Close Drawer 1','Open Drawer 2','Close Drawer 2',
 'Open Drawer 3','Close Drawer 3','Clean Table','Drink from Cup','Toggle Switch']
 
 
 
-
-
-
-
+## Define our DeepConvLSTM class, subclassing nn.Module.
 class DeepConvLSTM(nn.Module):
 
 	def __init__(self, n_hidden = 128, n_layers = 2, n_filters = 64,
@@ -57,23 +47,24 @@ class DeepConvLSTM(nn.Module):
 		self.n_filters = n_filters # number of convolutional filters per layer
 		self.n_classes = n_classes # number of target classes
 		self.filter_size = filter_size # convolutional filter size
+		self.pool_filter_size = pool_filter_size # max pool filter size if using
  
  		# Convolutional net
 		self.convlayer = nn.Sequential(
-			nn.Conv2d(n_channels, n_filters, (filter_size,1)),
+			nn.Conv1d(n_channels, n_filters, (filter_size)),
+			# nn.MaxPool2d((pool_filter_size,1)), # Max pool layers, optional. 
+			nn.Conv1d(n_filters, n_filters, (filter_size)),
 			# nn.MaxPool2d((pool_filter_size,1)),
-			nn.Conv2d(n_filters, n_filters, (filter_size,1)),
-			# nn.MaxPool2d((pool_filter_size,1)),
-			nn.Conv2d(n_filters, n_filters, (filter_size,1)),
-			nn.Conv2d(n_filters, n_filters, (filter_size,1))
+			nn.Conv1d(n_filters, n_filters, (filter_size)),
+			nn.Conv1d(n_filters, n_filters, (filter_size))
 			)
 
 		# LSTM layers
 		self.lstm = nn.LSTM(n_filters, n_hidden, n_layers, batch_first=True)
 
 		# Dropout layer
-		self.dropout = nn.Dropout2d(p=drop_prob)
-		self.dropout2 = nn.Dropout(p=drop_prob)
+		self.dropout2d = nn.Dropout2d(p=drop_prob)
+		self.dropout = nn.Dropout(p=drop_prob)
 
 		# Output layer
 		self.predictor = nn.Linear(n_hidden,n_classes)
@@ -82,15 +73,15 @@ class DeepConvLSTM(nn.Module):
 	def forward(self, x, hidden, batch_size):
 
 		#Reshape x if necessary to add the 2nd dimension
-		x = x.view(-1, n_channels, len_seq, 1)
-
+		x = x.view(-1, n_channels, len_seq)
+		# print(x.size())
 		x = self.convlayer(x)
-		x = self.dropout(x)
 		x = x.view(batch_size, -1, self.n_filters)
 		
 
 		x,hidden = self.lstm(x, hidden)
-		x = self.dropout2(x)
+
+		x = self.dropout(x)
 
 		x = x.view(batch_size, -1, self.n_hidden)[:,-1,:]
 		out = self.predictor(x)
@@ -99,37 +90,42 @@ class DeepConvLSTM(nn.Module):
 
 	def init_hidden(self, batch_size):
 
-		weight = next(self.parameters()).data
+		weight = next(self.parameters()).data # return a Tensor from self.parameters to use as a base for the initial hidden state.
 		
 		if (train_on_gpu):
-			hidden = (weight.new(self.n_layers, batch_size, self.n_hidden).zero_().cuda(),
-				  weight.new(self.n_layers, batch_size, self.n_hidden).zero_().cuda())
+			## Generate new tensors of zeros with similar type to weight, but different size.
+			hidden = (weight.new_zeros(self.n_layers, batch_size, self.n_hidden).cuda(), # Hidden state
+				  weight.new_zeros(self.n_layers, batch_size, self.n_hidden).cuda()) # Cell state
 		else:
-			hidden = (weight.new(self.n_layers, batch_size, self.n_hidden).zero_(),
-					  weight.new(self.n_layers, batch_size, self.n_hidden).zero_())
+			hidden = (weight.new_zeros(self.n_layers, batch_size, self.n_hidden),
+					  weight.new_zeros(self.n_layers, batch_size, self.n_hidden))
 		
 		return hidden
 
 
-
-def train(net, X_train, y_train,X_val,y_val, epochs=num_epochs, batch_size=batch_size, lr=0.01, remove_nulls=False, time=True, shuffle=True):
+def train(net, X_train, y_train,X_val,y_val, epochs=num_epochs, batch_size=batch_size, lr=lr, time=True, shuffle=True):
 
 	if time:
 		print('Starting training at',datetime.now())
 		start_time=datetime.now()
-
-	opt = torch.optim.SGD(net.parameters(), lr=lr, momentum=0.9, weight_decay=1e-4, nesterov=True) #Stochastic gradient descent optimiser with nesterov momentum
+	weight_decay = 1e-5*lr*batch_size*(50/batchlen)
+	opt = torch.optim.Adam(net.parameters(),lr=lr,weight_decay=weight_decay,amsgrad=True)
 	scheduler = torch.optim.lr_scheduler.StepLR(opt,100) # Learning rate scheduler to reduce LR every 100 epochs
-
-	criterion = nn.CrossEntropyLoss()
+	
 
 	if(train_on_gpu):
 		net.cuda()
 
-	print('Validation set statistics:')
-	print(len(np.unique([a for y in y_val for a in y])),np.unique([a for y in y_val for a in y],return_counts=True)[1])
+	train_stats = np.unique([a for y in y_train for a in y],return_counts=True)[1]
+	val_stats = np.unique([a for y in y_val for a in y],return_counts=True)[1]
+
 	print('Training set statistics:')
-	print(len(np.unique([a for y in y_train for a in y])),np.unique([a for y in y_train for a in y],return_counts=True)[1])
+	print(len(train_stats),'classes with distribution',train_stats)
+	print('Validation set statistics:')
+	print(len(val_stats),'classes with distribution',val_stats)
+	
+	criterion = nn.CrossEntropyLoss(weight=torch.tensor([max(train_stats)/i for i in train_stats]).cuda())
+	val_criterion = nn.CrossEntropyLoss()
 
 	early_stopping = EarlyStopping(patience=patience, verbose=False)
 
@@ -140,103 +136,86 @@ def train(net, X_train, y_train,X_val,y_val, epochs=num_epochs, batch_size=batch
 			train_losses = []
 			net.train()
 
+
 			h = net.init_hidden(batch_size)
+			for batch in iterate_minibatches_2D(X_train, y_train, batch_size, len_seq, stride, shuffle=shuffle, num_batches=num_batches, oversample=False, batchlen=batchlen, val=True):
 
-			for batch in iterate_minibatches_batched(X_train, y_train, batch_size, len_seq, stride, shuffle=True, num_batches=num_batches, oversample=True, batchlen=batchlen):
-				
+				x,y,pos= batch
 
-				x,y= batch
 
 				inputs, targets = torch.from_numpy(x), torch.from_numpy(y)
 				
-				
+
 				if(train_on_gpu):
-					inputs, targets = inputs.cuda(), targets.cuda()
+					targets = targets.cuda()
 				
+
+				opt.zero_grad() # Clear gradients in 
+
+				if pos==batchlen-1:
+					h = net.init_hidden(inputs.size()[0])
+
+				h = tuple([each.data for each in h])  # Get rid of gradients in hidden and cell states
+				if train_on_gpu:
+					inputs,targets = inputs.cuda(),targets.cuda()
 				
+				output, h = net(inputs,h,inputs.size()[0])
 
-				opt.zero_grad() # Clear gradients in opt
-
-				h = tuple([each.data for each in h])  # Get rid of gradients in hidden var from previous states
-
-				output = torch.FloatTensor().cuda()
-				targets_cumulative = torch.ByteTensor().cuda()
-
-				for j,i in enumerate(inputs):
-
-					
-					i = i.reshape((-1, len_seq, n_channels, 1))
-					out, h = net(i,h,inputs.size()[1])
-					output = torch.cat((output,out))
-					targets_cumulative = torch.cat((targets_cumulative, targets[j]))
-
-					
-
-				loss = criterion(output, targets_cumulative.long())
-				train_losses.append(loss.item())
+				loss = criterion(output, targets.long())
+				
 				loss.backward()
-				torch.nn.utils.clip_grad_norm_(net.parameters(), 0.5)
-
 				opt.step()	
 
+				if pos<batchlen:
+					torch.nn.utils.clip_grad_norm_(net.lstm.parameters(), 0.5) #Clip gradients during batch to prevent exploding gradient problem in lstms
+
+				train_losses.append(loss.item())
 
 		
 			val_losses = []
-			val_losses_weighted = []
 			net.eval()
 
 			top_classes = []
 			targets_cumulative = []
 			
-
-
-
+			val_h = net.init_hidden(val_batch_size)
 			with torch.no_grad():
-				for batch in iterate_minibatches_batched(X_val, y_val, val_batch_size, len_seq, stride, shuffle=True, num_batches=-1, batchlen=batchlen, val=True):
+				for batch in iterate_minibatches_2D(X_val, y_val, val_batch_size, len_seq, stride, shuffle=shuffle, num_batches=num_batches_val, batchlen=batchlen, val=True):
 					
 
-					x,y=batch
-						
+					x,y,pos=batch
 
 					inputs, targets = torch.from_numpy(x), torch.from_numpy(y)
 					
-					targets_cumulative.extend([y for y in y for y in y])
+					targets_cumulative.extend([y for y in y])
+
+					if pos == val_batch_size-1:
+						val_h = net.init_hidden(inputs.size()[0])
+
+					if train_on_gpu:
+						inputs,targets = inputs.cuda(),targets.cuda()
 					
+					output, val_h = net(inputs,val_h,inputs.size()[0])
+	
+					val_loss = val_criterion(output, targets.long())
+					val_losses.append(val_loss.item())
 
-					if(train_on_gpu):
-						inputs, targets = inputs.cuda(), targets.cuda()
-
-					val_h = net.init_hidden(inputs.size()[1])
-					val_h = tuple([each.data for each in val_h])
-
-
-					for j, i in enumerate(inputs):
-						
-						x = x.reshape((-1, len_seq, n_channels, 1))
-						output, val_h = net(i,val_h,inputs.size()[1])
-		
-						val_loss = criterion(output, targets[j].long())
-						val_losses.append(val_loss.item())
-
-						top_p, top_class = output.topk(1,dim=1)
-						top_classes.extend([top_class.item() for top_class in top_class.cpu()])
+					top_p, top_class = output.topk(1,dim=1)
+					top_classes.extend([top_class.item() for top_class in top_class.cpu()])
 
 			equals = [top_classes[i] == target for i,target in enumerate(targets_cumulative)]
 			accuracy = np.mean(equals)
 
 			f1score = metrics.f1_score(targets_cumulative, top_classes, average='weighted')
 			f1macro = metrics.f1_score(targets_cumulative, top_classes, average='macro')
-			balacc = metrics.balanced_accuracy_score(targets_cumulative, top_classes, adjusted=True)
 
 			
 			scheduler.step()
 
-			stopping_metric = (f1score+balacc-np.mean(val_losses))
-
-			print('Epoch {}/{}, Train loss: {:.4f}, Val loss: {:.4f}, Val f1: {:.4f}, M f1: {:.4f}, bal: {:.4f}, Metric: {:.4f}'.format(e+1,epochs,np.mean(train_losses),np.mean(val_losses),f1score,f1macro,balacc,stopping_metric))
+			print('Epoch {}/{}, Train loss: {:.4f}, Val loss: {:.4f}, Acc: {:.4f}, f1: {:.4f}, M f1: {:.4f}'.format(e+1,epochs,np.mean(train_losses),np.mean(val_losses),accuracy,f1score,f1macro))
 		
 
-			early_stopping((-stopping_metric), net)
+			early_stopping(np.mean(val_losses), net)
 
 			writer = csv.writer(csvfile, delimiter=' ',
 									quotechar='|', quoting=csv.QUOTE_MINIMAL)
@@ -275,41 +254,40 @@ def test(net, X_test, y_test, batch_size, remove_nulls=False, shuffle=True):
 	targets_cumulative = []
 	top_classes = []
 
-	with torch.no_grad():
+	test_h = net.init_hidden(test_batch_size)
 
-		for batch in iterate_minibatches_batched(X_test, y_test, test_batch_size, len_seq, stride, shuffle=True, num_batches=-1, batchlen=batchlen, val=True):
+	with torch.no_grad():
+			
+		for batch in iterate_minibatches_2D(X_test, y_test, test_batch_size, len_seq, stride, shuffle=True, num_batches=-1, batchlen=batchlen, val=True):
 
 			
 				
-			x,y=batch
+			x,y,pos=batch
 
 
 			
 			inputs, targets = torch.from_numpy(x), torch.from_numpy(y)
 
-			targets_cumulative.extend([y for y in y for y in y])
+			targets_cumulative.extend([y for y in y])
 
 
 			
 
 			if(train_on_gpu):
-				inputs, targets = inputs.cuda(), targets.cuda()
+				targets,inputs = targets.cuda(),inputs.cuda()
+
+			if pos == batchlen-1:
+				test_h = net.init_hidden(inputs.size()[0])
 
 
-			test_h = net.init_hidden(inputs.size()[1])
-			test_h = tuple([each.data for each in test_h])
 
-			for j, i in enumerate(inputs):
+			output, test_h = net(inputs,test_h,inputs.size()[0])
 
-				x = x.reshape((-1, len_seq, n_channels, 1))
+			val_loss = criterion(output, targets.long())
+			val_losses.append(val_loss.item())
 
-				output, test_h = net(i,test_h,inputs.size()[1])
-
-				val_loss = criterion(output, targets[j].long())
-				val_losses.append(val_loss.item())
-
-				top_p, top_class = output.topk(1,dim=1)
-				top_classes.extend([p.item() for p in top_class])
+			top_p, top_class = output.topk(1,dim=1)
+			top_classes.extend([p.item() for p in top_class])
 
 
 
@@ -321,17 +299,22 @@ def test(net, X_test, y_test, batch_size, remove_nulls=False, shuffle=True):
 	balacc = metrics.balanced_accuracy_score(targets_cumulative, top_classes,adjusted=True)
 
 	classreport = classification_report(targets_cumulative, top_classes,target_names=opp_class_names)
-	confmatrix = confusion_matrix(targets_cumulative, top_classes)
+	confmatrix = confusion_matrix(targets_cumulative, top_classes,normalize='true')
 	print('---- TESTING REPORT ----')
 	print(classreport)
-	print('---- CONFUSION MATRIX ----')
-	print(confmatrix)
 
+	df_cm = pd.DataFrame(confmatrix, index=opp_class_names,columns=opp_class_names)
+	plt.figure(10,figsize=(15,12))
+	sn.heatmap(df_cm,annot=True,fmt='.2f')
+	plt.savefig('{}_Confmatrix_{}_{}_{}_{}.png'.format(f1score,lr,batchlen,batch_size,weight_decay))
+	plt.show(plt.figure(10))
 	print('Testing balanced acc:',balacc)
 
 
 if __name__ == '__main__':
 
+	print('==HYPERPARAMETERS==')
+	print('learning rate',lr,'batch length',batchlen,'batch size',batch_size)
 
 	X_train,y_train = load_opp_runs('train',12,len_seq, stride)
 	X_val, y_val = load_opp_runs('val',3,len_seq, stride)
@@ -343,7 +326,7 @@ if __name__ == '__main__':
 	train_on_gpu = torch.cuda.is_available() # Check for cuda
 
 	try:
-		train(net,X_train,y_train,X_val,y_val, remove_nulls=False)
+		train(net,X_train,y_train,X_val,y_val, batch_size=batch_size, lr=lr)
 	except(KeyboardInterrupt):
 		pass
 
@@ -356,7 +339,7 @@ if __name__ == '__main__':
 
 	X_test, y_test = load_opp_runs('test',5,len_seq, stride)
 
-	print('Testing and saving fully trained model.')
+	print('Testing fully trained model.')
 
 	test(net, X_test,y_test, batch_size=batch_size, remove_nulls=False)
 
@@ -374,4 +357,4 @@ if __name__ == '__main__':
 
 	print('Plotting training curves')
 
-	plot_data(save_fig='{}-{}-{}'.format(len_seq,stride,num_epochs))
+	plot_data(save_fig='param_search/{}-{}-{}-{}'.format(lr,batchlen,batchsize,weight_decay))
